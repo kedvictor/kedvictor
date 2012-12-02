d5a439055aef87baa237ffbb806ab92634443c4a
kedvictor <kedvictor@gmail.com>
2012-12-02 08:51:28 +0200

    initial

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails' 
+  gem 'spork'
+  gem 'cucumber-rails'
+  gem 'webrat' 
+  gem 'capybara' 
+  gem 'database_cleaner' 
+  gem 'gherkin'  
+  gem 'launchy'    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kedvictor::Application.config.secret_token = '2d8cbca09d7d2abee88787868374503d6b1fdc43f5050611288fcf81865308ad0ffeec7fe13dd48ecf23d74b828ab25d211a277b8e28a19337dfbbe1c40da9ff'
