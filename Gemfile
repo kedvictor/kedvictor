af55e37e1c9f6582d713e4a94375415489696e6e
kedvictor <kedvictor@gmail.com>
2013-07-20 12:14:04 +0300

    database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.11'
 gem 'mysql2'
 gem 'pg'
+gem 'activerecord-postgresql-adapter'
 gem 'thin'
 gem 'resque', :require => "resque/server"
 gem 'redis'
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,14 +4,8 @@ class AuthController < ApplicationController
     @agents = Agent.all    
   end
   
-  def ident
-    guid = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-    cookies[:guid] = {
-      :value => guid,
-      :expire => 3.years.from_now
-    }
-    Agent.create :guid => guid
-    render :text => "document.cookie='guid=112233445566778899'";
+  def ident   
+    
   end
   
 end
